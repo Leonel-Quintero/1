1652ccf2a8280295e8381c84314833475c839d86
Leonel Quintero <leonelbenjaminquintero@gmail.com>
2026-04-01 00:21:31 -0300

    Cambios en Dev-2

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 
-void primero ();
-void segundo ();
+void primero (int n1);
+void segundo (int n2);
 void iguales ();
 
 int main(){
+    int a, b;
 
+
+
+
+
+
+
+
+
+
+
+    if (a > b) {
+        primero(a);
+    } else if (b > a) {
+        segundo(b);
+    } else {
+        iguales();
+    }
 }
