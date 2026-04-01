2b5a4e5b49aafd1e8082a1c0b3756a90b116c19b
Leonel Quintero <leonelbenjaminquintero@gmail.com>
2026-04-01 00:25:04 -0300

    Cambios en Dev-3

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
-void primero ();
-void segundo ();
+void primero (int n1);
+void segundo (int n2);
 void iguales ();
 
 int main(){
 
+}
+void primero(int n1) {
+    printf("El primero (%d) es el mayor.\n", n1);
+}
+
+void segundo(int n2) {
+    printf("El segundo (%d) es el mayor.\n", n2);
+}
+
+void iguales() {
+    printf("Ambos numeros son iguales.\n");
 }
