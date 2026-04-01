4cf4c04804638c4cff11bc5a66e3ca878d1a914d
marianocaviola <caviolamariano@gmail.com>
2026-04-01 00:20:53 -0300

    Ingreso de numeros

--- main.c
+++ main.c
@@ -5,5 +5,10 @@ void segundo ();
 void iguales ();
 
 int main(){
-
+    int a, b;
+    printf("Ingrese el primer numero: ");
+    scanf("%d", &a);
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &b);
+    return 0;
 }
